83a98013090d0d88a321cc41e722e1f9900475b8
Vishal Pant <vishal.pant1@gmail.com>
2026-01-18 17:41:56 +0530

    modal project updates

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+function showModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function hideModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', showModal);
+}
+
+btnCloseModal.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
+
+/* document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') hideModal();
+}); */
+
+document.addEventListener(
+  'keydown',
+  e => e.key === 'Escape' && !modal.classList.contains('hidden') && hideModal()
+);
